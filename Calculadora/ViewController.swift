e876f9d228bc71c1e8925e7c3341856b21e7ff08
Hermivaldo Oliveira <hermivaldo.braga@gmail.com>
2018-03-07 17:09:16 -0300

    primeira versão de teste.

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -10,6 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbConta: UILabel!
+    
+    @IBAction func btnClickNumber(_ sender: UIButton) {
+        
+        if (lbConta.text!.elementsEqual("0")){
+            lbConta.text = sender.titleLabel?.text!
+        }else{
+            
+            lbConta.text = "\(lbConta.text!)\(sender.titleLabel!.text!)"
+        }
+        
+    }
+    
+    @IBAction func zerarValores(_ sender: Any) {
+        lbConta.text = "0"
+    }
+    
+    @IBAction func btnOperation(_ sender: UIButton) {
+        let operationSignal = sender.titleLabel?.text!
+        let wordList = ["+", "-", "*", "/"]
+        var operationExists: String = ""
+        let sucess = wordList.contains(where: {
+            if (lbConta.text!.contains($0) == true){
+                operationExists = $0
+            }
+            return lbConta.text!.contains($0)
+        });
+        
+        if (operationSignal!.elementsEqual("=")){
+            if (sucess){
+                var resultado = 0.0
+                switch operationExists {
+                case "+":
+                     resultado = lbConta.text!.split(separator: operationExists.first!).reduce(0, {$0 + Double($1)!})
+                case "-":
+                     resultado = lbConta.text!.split(separator: operationExists.first!).reduce(0, {$0 - Double($1)!})
+                case "*":
+                     resultado = lbConta.text!.split(separator: operationExists.first!).reduce(0, { Double($1)! * $0 })
+                case"/":
+                     resultado = lbConta.text!.split(separator: operationExists.first!).reduce(0, { Double($1)! / $0})
+                default:
+                    resultado = 0.0
+                }
+                
+                
+                lbConta.text = "\(resultado)"
+            }
+        }else{
+            
+            
+            if (!sucess){
+                lbConta.text = "\(lbConta.text!)\(operationSignal!)"
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
